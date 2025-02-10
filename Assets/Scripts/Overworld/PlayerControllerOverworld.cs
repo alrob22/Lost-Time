10eb51a0c432d0c01c85b29673ef71a0e289f075
Aden Robertson <alrob2224@gmail.com>
2025-02-10 15:30:42 -0500

    Added initial overworld
    We wanted a curved world that hides locales from the player, nudging them to explore the overworld. I found this shader that mimics Animal Crossing's curved world and used it for our overworld: https://alastaira.wordpress.com/2013/10/25/animal-crossing-curved-world-shader/
    
    The shader curves anything that is attached to it along the camera. I also updated player controls specifically for the overworld and made them feel like Final Fantasy 9 tank controls to simulate that game's traversal.

---
+++ Assets/Scripts/Overworld/PlayerControllerOverworld.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerOverworld : MonoBehaviour
+{
+    [Header("References")]
+    private CharacterController controller;
+
+    [Header("Movement Settings")]
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 10f;
+    public float sprintTransitSpeed = 5f;
+    public float turnSpeed = 2f;
+    public float gravity = 9.81f;
+
+    private float speed;
+    private float verticalVelocity;
+
+    [Header("Input")]
+    private float verticalMoveInput;
+    private float horizontalMoveInput;
+    private float turnInput;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        InputManagement();
+        Movement();
+    }
+
+    void InputManagement()
+    {
+        // Get input from the player
+        verticalMoveInput = Input.GetAxis("Vertical");
+        horizontalMoveInput = Input.GetAxis("Horizontal");
+        turnInput = 0f;
+
+        // Handle turn input for Q (left) and E (right)
+        if (Input.GetKey(KeyCode.Q))
+        {
+            turnInput = -1f; // Rotate left
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            turnInput = 1f; // Rotate right
+        }
+    }
+
+    void Movement()
+    {
+        GroundMovement();
+        Turn();
+    }
+
+    void GroundMovement()
+    {
+        Vector3 move = new Vector3(horizontalMoveInput, 0, verticalMoveInput);
+        move = transform.TransformDirection(move);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = Mathf.Lerp(speed, sprintSpeed, sprintTransitSpeed * Time.deltaTime);
+        } else
+        {
+            speed = Mathf.Lerp(speed, walkSpeed, sprintTransitSpeed * Time.deltaTime);
+        }
+
+        move *= speed;
+        move.y = VerticalForceCalculation();
+
+        controller.Move(move * Time.deltaTime);
+    }
+
+    void Turn()
+    {
+        if (turnInput != 0)
+        {
+            // Rotate the player around the Y-axis
+            transform.Rotate(Vector3.up * turnInput * turnSpeed);
+        }
+    }
+
+    float VerticalForceCalculation()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1f;
+        } else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        return verticalVelocity;
+    }
+}
